ee7c6b7f92326be011da91badefbb998e187c6e7
Leyberson <leyberson1@gmail.com>
2019-06-30 07:52:59 -0300

    Consulta versao final e funcionando

--- model/Consulta.java
+++ model/Consulta.java
@@ -3,39 +3,27 @@ package model;
 import controller.*;
 import view.*;
 
-import java.util.Calendar;
+import java.time.LocalDateTime;
+
 
 public class Consulta implements java.io.Serializable{
 	Paciente paciente;
 	Medico medico;
-	Calendar calendar = Calendar.getInstance();
+	LocalDateTime horario;
 
 	public Consulta (Paciente paciente, Medico medico, int dia, int mes, int ano, int hora){
 		this.paciente = paciente;
 		this.medico = medico;
-		calendar.set(Calendar.DAY_OF_MONTH, dia);
-		calendar.set(Calendar.MONTH, mes);
-		calendar.set(Calendar.YEAR, ano);
-		calendar.set(Calendar.HOUR_OF_DAY, hora);
-	}
-
-	public String toString(){
-		return calendar.get(Calendar.DAY_OF_MONTH) +"/"
-		+calendar.get(Calendar.MONTH) +"/"
-		+calendar.get(Calendar.YEAR) +" as "
-		+calendar.get(Calendar.HOUR_OF_DAY);
-	}
-
-	public boolean equals(Consulta outra){
-		return ((this.paciente.equals(outra.paciente) && this.medico.equals(outra.medico)) && 
-		(calendar.get(Calendar.DAY_OF_MONTH) == outra.calendar.get(Calendar.DAY_OF_MONTH)) &&
-		(calendar.get(Calendar.MONTH) == outra.calendar.get(Calendar.MONTH)) &&
-		(calendar.get(Calendar.YEAR) == outra.calendar.get(Calendar.YEAR)) &&
-		(calendar.get(Calendar.HOUR_OF_DAY) == outra.calendar.get(Calendar.HOUR_OF_DAY)));
-	}
+		horario = LocalDateTime.of(ano, mes, dia, hora, 0);
+        }
         
-        public boolean depois(Calendar data){
-            return calendar.after(data);
+        public LocalDateTime getHorario(){
+            return this.horario;
+        }
+        
+        public boolean equals(Consulta outra){
+            return (this.paciente.equals(outra.paciente) && this.medico.equals(outra.medico) &&
+                    this.horario.equals(outra.horario));
         }
         
 }
